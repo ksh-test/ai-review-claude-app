9e0616fa94fde6134e151cbd6aad91da5e1d4faa
김성현/인프라플랫폼개발팀/CL <sunghyun.kim@nhn.com>
2025-10-01 17:51:07 +0900

    spring boot init

---
+++ src/main/java/com/ksh/toy/aiprreviewtest/AiPrReviewTestApplication.java
@@ -0,0 +1,13 @@
+package com.ksh.toy.aiprreviewtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPrReviewTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AiPrReviewTestApplication.class, args);
+  }
+
+}
